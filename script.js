86f01151d21da126b88cea76527682880fcc8199
Glitch (hello-webpage) <none>
2021-06-30 09:42:32 +0000

    📮🎩 Checkpoint ./script.js:6330142/329

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const scopes = ['payments'];
 
+var authResult = null;
 
 function auth(){
   window.Pi.authenticate(scopes, onIncompletePaymentFound)
-  .then(function(auth) {
+  .then(function(_authResult) {
+    authResult = _authResult;
     console.log("Hi there! You're ready to make payments!");
     window.alert("Hi there! You're ready to make payments!");
     document.getElementById("authButtonId").disabled=true;
-    document.getElementById("authId").innerHTML="Authentication ok!"
+    document.getElementById("authId").innerHTML="Welcome"+authResult.user.username;
   })
   .catch(function(error) {
     console.error(error);
