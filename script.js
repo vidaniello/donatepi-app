1c30603a4a45a67949099f263e9fefeab4b48bc1
Glitch (donatepi) <none>
2021-06-30 10:53:28 +0000

    👟⛺️ Checkpoint ./index.html:6330142/1 ./script.js:6330142/747

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function auth(){
     //console.log("Hi there! You're ready to make payments!");
     //window.alert("Hi there! You're ready to make payments!");
     document.getElementById("authButtonId").remove();
-    document.getElementById("authId").innerHTML="Welcome "+authResult.user.username/*+", accessToken: "+authResult.accessToken*/;
+    document.getElementById("authId").innerHTML="Welcome "+authResult.user.username+", authentication ok!"/*+", accessToken: "+authResult.accessToken*/;
   })
   .catch(function(error) {
     console.error(error);
@@ -23,3 +23,23 @@ function onIncompletePaymentFound(payment) {
   console.log("Incomplete payment not found: "+payment);
   window.alert("Incomplete payment not found: "+payment);
 };
+
+
+
+
+function createPay(){
+  window.Pi.createPayment({
+  // Amount of π to be paid:
+  amount: 5,
+  // An explanation of the payment - will be shown to the user:
+  memo: "a memo.....", // e.g: "Digital kitten #1234",
+  // An arbitrary developer-provided metadata object - for your own usage:
+  metadata: { cause:"donation"}, // e.g: { kittenId: 1234 }
+}, {
+  // Callbacks you need to implement - read more about those in the detailed docs linked below:
+  onReadyForServerApproval: function(paymentId) { /* ... */ },
+  onReadyForServerCompletion: function(paymentId, txid) { /* ... */ },
+  onCancel: function(paymentId) { /* ... */ },
+  onError: function(error, payment) { /* ... */ },
+});
+}
