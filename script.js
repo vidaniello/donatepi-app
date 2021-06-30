a3a450e32ba94c4e62a9df89d1b5b5879b20bdfe
Glitch (hello-webpage) <none>
2021-06-30 08:55:20 +0000

    🏗💶 Checkpoint ./script.js:6330142/919 ./index.html:6330142/145

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Ciao!");
+const scopes = ['payments'];
+
+
+function auth(){
+  window.Pi.authenticate(scopes, onIncompletePaymentFound)
+  .then(function(auth) {
+    console.log("Hi there! You're ready to make payments!");
+    window.alert("Hi there! You're ready to make payments!");
+    document.getElementById("authButtonId");
+  })
+  .catch(function(error) {
+    console.error(error);
+    window.alert("Authentication failed!"+error);
+  });
+}
+
+
+
+function onIncompletePaymentFound(payment) { 
+  console.log("Incomplete payment not found: "+payment);
+  window.alert("Incomplete payment not found: "+payment);
+};
